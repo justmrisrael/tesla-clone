ef68de75fb598a8893b8612473f317af66de453c
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2021-11-20 09:47:15 -0600

    master

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+
+
     return (
         <Container>
             <a>
@@ -17,9 +21,28 @@ function Header() {
 
             <RightMenu>
                 <a href = "#">Shop</a>
-                <a href = "#">Tesla Account</a>
-                <CustomMenu />
+                <a href = "#">Account</a>
+                <CustomMenu onClick = {() => setBurgerStatus(true)}/>
             </RightMenu>
+            <BurgerNav show = {burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick = {() => setBurgerStatus(false)}/>
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+                <li><a href="#">Semi</a></li>
+                <li><a href="#">Charging</a></li>
+                <li><a href="#">Powerwall</a></li>
+                <li><a href="#">Commercial Energy</a></li>
+                <li><a href="#">Utilities</a></li>
+                <li><a href="#">Find Us</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">Investor Relations</a></li>
+
+            </BurgerNav>
 
         </Container>
     )
@@ -31,12 +54,13 @@ const Container = styled.div`
     min-height: 60px;
     position: fixed;
     display: flex;
-    algin-items: center;
+    align-items: center;
     justify-content: space-between;
     padding: 0 20px;
     top: 0;
     left: 0;
     right: 0;
+    z-index: 1;
 `
 const Menu = styled.div`
     display: flex;
@@ -69,4 +93,41 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
     cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 16;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.6s;
+    li{
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, .2);
+
+        a{
+            font-weight: 600;
+        }
+    }
+`
+
+const CustomClose = styled(CloseIcon)`
+
+`
+
+const CloseWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    a {
+        text-transform: normal;
+    }
 `
